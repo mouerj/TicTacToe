d711a819a644a9624adbfefb3cb950b1df48f97d
richardgmartin <richardgeorgemartin@gmail.com>
2016-01-14 15:37:20 -0600

    working initial x-based array method

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -10,6 +10,32 @@
 
 @interface ViewController ()
 
+// row 0 buttons
+@property (weak, nonatomic) IBOutlet UIButton *button00;
+
+@property (weak, nonatomic) IBOutlet UIButton *button01;
+
+@property (weak, nonatomic) IBOutlet UIButton *button02;
+
+// row 1 buttons
+
+@property (weak, nonatomic) IBOutlet UIButton *button10;
+
+@property (weak, nonatomic) IBOutlet UIButton *button11;
+
+@property (weak, nonatomic) IBOutlet UIButton *button12;
+
+// row 2 buttons
+
+@property (weak, nonatomic) IBOutlet UIButton *button20;
+
+@property (weak, nonatomic) IBOutlet UIButton *button21;
+
+@property (weak, nonatomic) IBOutlet UIButton *button22;
+
+
+
+
 @end
 
 @implementation ViewController
@@ -17,11 +43,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)onButtonPressed:(UIButton *)sender {
+    
+    
+    [sender setTitle:@"X" forState:UIControlStateNormal];
+    
+    NSMutableArray *ticTac = @[ @[ @"button00", @"button01", @"button02" ],
+                                @[ @"button10", @"button11", @"button12" ],
+                                @[ @"button20", @"button21", @"button22" ] ];
+    
+    
+    
+    
+
+    
+    
+    
+    
 }
+
+
+
 
 @end
