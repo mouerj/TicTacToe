7b541920d22d26790849d1289ceb714c1ce90164
richardgmartin <richardgeorgemartin@gmail.com>
2016-01-16 14:26:56 -0600

    completed winner determination logic

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -14,29 +14,37 @@
 
 // row 0 buttons
 @property (weak, nonatomic) IBOutlet UIButton *button00;
+@property (weak, nonatomic) NSString  *button00String;
 
 @property (weak, nonatomic) IBOutlet UIButton *button01;
+@property (weak, nonatomic) NSString  *button01String;
 
 @property (weak, nonatomic) IBOutlet UIButton *button02;
+@property (weak, nonatomic) NSString  *button02String;
 
 // row 1 buttons
 
 @property (weak, nonatomic) IBOutlet UIButton *button10;
+@property (weak, nonatomic) NSString  *button10String;
 
 @property (weak, nonatomic) IBOutlet UIButton *button11;
+@property (weak, nonatomic) NSString  *button11String;
 
 @property (weak, nonatomic) IBOutlet UIButton *button12;
+@property (weak, nonatomic) NSString  *button12String;
 
 // row 2 buttons
 
 @property (weak, nonatomic) IBOutlet UIButton *button20;
+@property (weak, nonatomic) NSString  *button20String;
 
 @property (weak, nonatomic) IBOutlet UIButton *button21;
+@property (weak, nonatomic) NSString  *button21String;
 
 @property (weak, nonatomic) IBOutlet UIButton *button22;
+@property (weak, nonatomic) NSString  *button22String;
 
 @property (nonatomic) BOOL move;
-
 
 @property (weak, nonatomic) IBOutlet UILabel *winnerLabel;
 
@@ -47,10 +55,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
     self.move = YES;
-   // self.whichPlayerLabel.text = @"X";
+    
+    self.winnerLabel.text = @"Winner: TBA";
+    
+    // assignment of button currentTitle to corresponding
+    
+    self.button00String = @"00";
+    self.button01String = @"01";
+    self.button02String = @"02";
+    
+    self.button10String = @"10";
+    self.button11String = @"11";
+    self.button12String = @"12";
+    
+    self.button20String = @"20";
+    self.button21String = @"21";
+    self.button22String = @"22";
+    
+    
 }
 
 - (IBAction)onButtonPressed:(UIButton *)sender {
@@ -60,57 +84,110 @@
         [sender setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
         self.whichPlayerLabel.text = @"O";
         self.move = NO;
+        
+        if ([sender.titleLabel.text isEqualToString:@"00"]) {
+            self.button00String = @"X";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"01"]) {
+            self.button01String = @"X";
+        }
+
+        if ([sender.titleLabel.text isEqualToString:@"02"]) {
+            self.button02String = @"X";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"10"]) {
+            self.button10String = @"X";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"11"]) {
+            self.button11String = @"X";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"12"]) {
+            self.button12String = @"X";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"20"]) {
+            self.button20String = @"X";
+        }
+        if ([sender.titleLabel.text isEqualToString:@"21"]) {
+            self.button21String = @"X";
+        }
+        if ([sender.titleLabel.text isEqualToString:@"22"]) {
+            self.button22String = @"X";
+        }
+        
     }
     else {
         [sender setTitle: @"O" forState:UIControlStateNormal];
         [sender setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
         self.whichPlayerLabel.text = @"X";
         self.move = YES;
+        
+        if ([sender.titleLabel.text isEqualToString:@"00"]) {
+            self.button00String = @"O";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"01"]) {
+            self.button01String = @"O";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"02"]) {
+            self.button02String = @"O";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"10"]) {
+            self.button10String = @"O";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"11"]) {
+            self.button11String = @"O";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"12"]) {
+            self.button12String = @"O";
+        }
+        
+        if ([sender.titleLabel.text isEqualToString:@"20"]) {
+            self.button20String = @"O";
+        }
+        if ([sender.titleLabel.text isEqualToString:@"21"]) {
+            self.button21String = @"O";
+        }
+        if ([sender.titleLabel.text isEqualToString:@"22"]) {
+            self.button22String = @"O";
+        }
+
     }
 
-    NSLog(@"%@", sender.currentTitle);
-         
-    // assignment of button currentTitle to corresponding
+    NSLog(@"sender.currentTitle is: %@", sender.currentTitle);
+    NSLog(@"sender.titleLabel.text is: %@", sender.titleLabel.text);
+    // NSLog(@"self.button00String is: %@", self.button00String);
     
-    NSString *button00String = self.button00.currentTitle;
-    NSString *button01String = self.button01.currentTitle;
-    NSString *button02String = self.button02.currentTitle;
-    
-    NSString *button10String = self.button10.currentTitle;
-    NSString *button11String = self.button11.currentTitle;
-    NSString *button12String = self.button12.currentTitle;
-    
-    NSString *button20String = self.button20.currentTitle;
-    NSString *button21String = self.button21.currentTitle;
-    NSString *button22String = self.button22.currentTitle;
     
     // logic to determine winner
     
     
     if (
-        ([button00String isEqualToString:button01String] && [button01String isEqualToString:button02String]) ||
-        ([button10String isEqualToString:button11String] && [button11String isEqualToString:button12String]) ||
-        ([button20String isEqualToString:button21String] && [button21String isEqualToString:button22String]) ||
-        ([button00String isEqualToString:button10String] && [button10String isEqualToString:button20String]) ||
-        ([button01String isEqualToString:button11String] && [button11String isEqualToString:button21String]) ||
-        ([button02String isEqualToString:button12String] && [button12String isEqualToString:button22String])
+        ([self.button00String isEqualToString:self.button01String] && [self.button01String isEqualToString:self.button02String]) ||
+        ([self.button10String isEqualToString:self.button11String] && [self.button11String isEqualToString:self.button12String]) ||
+        ([self.button20String isEqualToString:self.button21String] && [self.button21String isEqualToString:self.button22String]) ||
+        ([self.button00String isEqualToString:self.button10String] && [self.button10String isEqualToString:self.button20String]) ||
+        ([self.button01String isEqualToString:self.button11String] && [self.button11String isEqualToString:self.button21String]) ||
+        ([self.button02String isEqualToString:self.button12String] && [self.button12String isEqualToString:self.button22String]) ||
+        ([self.button00String isEqualToString:self.button11String] && [self.button11String isEqualToString:self.button22String]) ||
+        ([self.button02String isEqualToString:self.button11String] && [self.button11String isEqualToString:self.button20String])
         )
         
     {
-        self.winnerLabel.text = @"Player <> is the Winner";
+        NSString *theWinner = [NSString stringWithFormat:@"Player %@ is the Winner", sender.currentTitle];
+        
+        self.winnerLabel.text = theWinner;
+        
     }
-    
-    
-    
-    
-    // NSLog(@"value of self.winnerLabel.text is: %@", self.winnerLabel.text);
-    //    NSString *test1 = self.button00.currentTitle;
-    //
-    //    NSString *test2 = sender.titleLabel.text;
-    //
-    //    NSLog(@"value of self.button00.currentTitle for button00 is: %@", test1);
-    //
-    //    NSLog(@"value of sender.titleLabel.text for button00 is: %@", test2);
+
 
 }
 
